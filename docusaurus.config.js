e249c708c66b1f87d5c9d0b10a5ae34b9d9b2192
Mark O'Neil <zeretic@gmail.com>
2023-06-25 10:15:02 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   // Set the production url of your site here
   // url: 'https://blackboard.github.io',
   // url: 'https://anthologydevdocs.github.io',
-  url: 'https://docs.anthology.com'
+  url: 'https://docs.anthology.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/',
