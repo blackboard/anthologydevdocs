c671e4822193747c166ef2d53b092bd26f7ff9f9
Sebastian Silva <44152511+OneComputerGuy@users.noreply.github.com>
2026-09-07 14:55:58 -0500

    Added redirect to moved pages

--- page-redirects.js
+++ page-redirects.js
@@ -2,6 +2,14 @@ const customRedirects = {
   "old-slug": "new-slug",
   "/docs/developer-ami": "/docs/blackboard/sandbox-envs/welcome",
   "/docs/developer-amifromvmdk": "/docs/blackboard/sandbox-envs/welcome",
+  "/docs/blackboard/uef/getting-started":
+    "/docs/blackboard/uef/Start%20Developing%20for%20uef/getting-started",
+  "/docs/blackboard/uef/how-it-works":
+    "/docs/blackboard/uef/Start%20Developing%20for%20uef/how-it-works",
+  "/docs/blackboard/uef/uef-requirements":
+    "/docs/blackboard/uef/Start%20Developing%20for%20uef/uef-requirements",
+  "/docs/blackboard/uef/uef-authentication":
+    "/docs/blackboard/uef/Start%20Developing%20for%20uef/uef-authentication",
 };
 
 module.exports = customRedirects;
